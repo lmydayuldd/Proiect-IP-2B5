b92fd449279e6a6dc3165f56269418841763a598
alexandru <juve45@rocketmail.com>
2017-05-08 10:40:15 +0300

    Added comments

--- MinDistancePath.java
+++ MinDistancePath.java
@@ -1,10 +1,21 @@
 package modul3;
 
+import java.util.ArrayList;
+
 
 public class MinDistancePath extends PathFinder {
 
+    public MinDistancePath(Matrix m) {
+        super(m);
+    }
+
     @Override
     public void execute(Cell a, Cell b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void execute(ArrayList<Cell> path) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
--- SimplePath.java
+++ SimplePath.java
@@ -1,9 +1,20 @@
 package modul3;
 
+import java.util.ArrayList;
+
 public class SimplePath extends PathFinder {
+
+    public SimplePath(Matrix m) {
+        super(m);
+    }
 
     @Override
     public void execute(Cell a, Cell b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void execute(ArrayList<Cell> path) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
