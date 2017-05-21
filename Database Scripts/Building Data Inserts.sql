4defdcf0c8d597ef125d1a11c308c54610f6da75
Razvan Romanescu <romanescu.raz@gmail.com>
2017-05-21 11:57:59 +0300

    Created two scripts for BUILDING_DATA
    Building Data script creates the table and its trigger as seen in the
    other examples and Building Data Inserts populates the table with a
    couple of elements in order to test the XML function

---
+++ Database Scripts/Building Data Inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO BUILDING_DATA(elementType, x1, y1, x2, y2, floor, room, isExterior, isExit) VALUES('Wall', 12, 18, -32, 80, 1, 112, 0, 0);
+INSERT INTO BUILDING_DATA(elementType, x1, y1, x2, y2, floor, room, isExterior, isExit) VALUES('Door', 5, 5, -5, -7, 5, 509, 0, 1);
+INSERT INTO BUILDING_DATA(elementType, x1, y1, x2, y2, floor, room, isExterior, isExit) VALUES('Stairs', 54, 23, 14, -8, 2, 0, 0, 0);
+INSERT INTO BUILDING_DATA(elementType, x1, y1, x2, y2, floor, room, isExterior, isExit) VALUES('Window', 4, 4, 3, 3, 3, 302, 1, 0);
+INSERT INTO BUILDING_DATA(elementType, x1, y1, x2, y2, floor, room, isExterior, isExit) VALUES('Wall', 90, 65, -43, 34, 1, 112, 1, 1);
