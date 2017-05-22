58775e7115e1ae90ae4ed22248968d7dbe4b3e2e
Andreea Borcan <andreea_borcan96@yahoo.ro>
2017-05-22 10:39:22 +0300

    added comments

--- app/XMLInput.java
+++ app/XMLInput.java
@@ -7,24 +7,31 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * Created by Vasile Catana on 5/20/2017.
+ * This class recieves a byte array and interprets it as xml file.
  */
 
 
 public class XMLInput  extends ByteArrayInputStream{
     private DataInputStream dataInputStream;
 
-    public XMLInput(InputStream inputStream)
-    {
+    public XMLInput(InputStream inputStream){
         super(new byte[11]); // harcoded
         this.dataInputStream = new DataInputStream(inputStream);
     }
 
+    /**
+    * reads binary data from socket
+    */
     private void receive() throws IOException {
         byte[] data = new byte[500]; //harcoded .. to change..
         this.dataInputStream.read(data,0,500);
         this.buf = data;
     }
 
+
+    /**
+    * Saves bytes recieved via @param inputStream in file @param pathToSave
+    */
     public static void getXMLFile(InputStream inputStream, String pathToSave) throws ParserConfigurationException, IOException, SAXException {
         XMLInput xmlInput = new XMLInput(inputStream);
         xmlInput.receive();
