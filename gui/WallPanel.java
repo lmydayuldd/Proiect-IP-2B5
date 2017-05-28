416f370c9cbf48a8fb73f6b7f2945c2b4368752f
alexandru <juve45@rocketmail.com>
2017-05-28 19:10:08 +0300

    Added first buttons in GUI

--- gui/WallPanel.java
+++ gui/WallPanel.java
@@ -20,29 +20,38 @@ class WallPanel extends JPanel{
 
     JComboBox <String> componentType;
     Border border = new BevelBorder(1);
-    JLabel topLeftLabel, bottomRightLabel;
+    JLabel topLeftLabel, bottomRightLabel, wallHelpLabel;
     JTextField x1TextField, y1TextField, x2TextField, y2TextField;
+    JButton addWall;
     
     public WallPanel() {
         setLayout(new FlowLayout());
         topLeftLabel = new JLabel("TopLeft Coordinates: ");
-        topLeftLabel.setForeground(Color.LIGHT_GRAY);
+        //topLeftLabel.setForeground(Color.LIGHT_GRAY);
         bottomRightLabel = new JLabel("Bottom-Right Coordinates");
-        bottomRightLabel.setForeground(Color.LIGHT_GRAY);
-        x1TextField = new JTextField("X   ");
-        y1TextField = new JTextField("Y   ");
-        x2TextField = new JTextField("X   ");
-        y2TextField = new JTextField("Y   ");
+        //bottomRightLabel.setForeground(Color.LIGHT_GRAY);
+        wallHelpLabel = new JLabel("<html><h1>Add Wall<br></h1>Fill up this form in order to add a wall to the Application. </html>");
+        x1TextField = new JTextField("X");
+        x1TextField.setPreferredSize(new Dimension(50, 20));
+        y1TextField = new JTextField("Y");
+        y1TextField.setPreferredSize(new Dimension(50, 20));
+        x2TextField = new JTextField("X");
+        x2TextField.setPreferredSize(new Dimension(50, 20));
+        y2TextField = new JTextField("Y");
+        y2TextField.setPreferredSize(new Dimension(50, 20));
+        addWall = new JButton("Add Wall");
         
+        add(wallHelpLabel);
         add(topLeftLabel);
         add(x1TextField);
         add(y1TextField);
         add(bottomRightLabel);
         add(x2TextField);
         add(y2TextField);
+        add(addWall);
         setPreferredSize(new Dimension(1024, 250));
         setBorder(border);
-        setBackground(Color.DARK_GRAY);
+        //setBackground(Color.DARK_GRAY);
     }
     
     
