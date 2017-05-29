f9a57892f08049071138cf7fedc4da0006ca8f84
alexandru <juve45@rocketmail.com>
2017-05-29 10:16:20 +0300

    a

--- gui/WallPanel.java
+++ gui/WallPanel.java
@@ -8,9 +8,18 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Vector;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
 
 /**
@@ -24,9 +33,11 @@ class WallPanel extends JPanel{
     JComboBox <String> componentType;
     Border border = new BevelBorder(1);
     JLabel topLeftLabel, bottomRightLabel, wallHelpLabel;
-    JTextField x1TextField, y1TextField, x2TextField, y2TextField;
+    JTextField x1TextField, y1TextField, x2TextField, y2TextField, floorField;
     JButton addWall;
-    
+    JTable walls;
+    JScrollPane tableScrollPane = new JScrollPane();
+    DefaultTableModel wallsTableModel;
     /**
      * Split the gui in three pane's to arrange them nicely.
      */
@@ -45,6 +56,8 @@ class WallPanel extends JPanel{
         wallHelpLabel = new JLabel("<html><h1>Add Wall<br></h1>Fill up this form in order to add a wall to the Application. </html>");
         x1TextField = new JTextField("X");
         x1TextField.setPreferredSize(new Dimension(50, 20));
+        floorField = new JTextField("Floor");
+        floorField.setPreferredSize(new Dimension(50, 20));
         y1TextField = new JTextField("Y");
         y1TextField.setPreferredSize(new Dimension(50, 20));
         x2TextField = new JTextField("X");
@@ -57,7 +70,7 @@ class WallPanel extends JPanel{
         topPane = new JPanel();
         topPane.setPreferredSize(new Dimension(1000, 100));
         addWallPane = new JPanel();
-        addWallPane.setPreferredSize(new Dimension(1000, 100));
+        addWallPane.setPreferredSize(new Dimension(1000, 50));
         removeWallPane = new JPanel();
         
         //Adding components of topPane
@@ -66,6 +79,7 @@ class WallPanel extends JPanel{
         add(topPane);
         
         //Adding components of add Wall Pane
+        addWallPane.add(floorField);
         addWallPane.add(topLeftLabel);
         addWallPane.add(x1TextField);
         addWallPane.add(y1TextField);
@@ -79,8 +93,52 @@ class WallPanel extends JPanel{
         
         //TODO remove Pane!!
         
-        setPreferredSize(new Dimension(1024, 250));
+        removeWallPane = new JPanel();
+        
+
+        
+        //setting up the bottom table
+        String tableHeader[] = {"Floor", "X1", "Y1", "X2", "Y2"};
+        wallsTableModel = new DefaultTableModel(null, tableHeader);
+        wallsTableModel.addRow(tableHeader);
+        walls = new JTable(wallsTableModel);
+        walls.setPreferredScrollableViewportSize(new Dimension(500, 100));
+
+        /*
+        //Setting up the scroll pane in which we will put the Table
+        tableScrollPane.setPreferredSize(new Dimension(600, 100));
+        tableScrollPane.add(walls);
+        */
+        removeWallPane.add(new JScrollPane(walls));
+        
+        
+        //Adding to the main frame the bottom panel which contains the table
+        // with the walls data
+        add(removeWallPane);
+        setPreferredSize(new Dimension(1024, 450));
         setBorder(border);
         //setBackground(Color.DARK_GRAY);
+        
+        addListeners();
+    }
+    
+    private void addListeners()
+    {
+                addWall.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    selectionButtonPressed();
+            }
+
+            private void selectionButtonPressed() {
+                
+                String myRow[] = new String[5];
+                myRow[0] = floorField.getText();
+                myRow[1] = x1TextField.getText();
+                myRow[2] = y1TextField.getText();
+                myRow[3] = x2TextField.getText();
+                myRow[4] = y2TextField.getText();
+                wallsTableModel.addRow(myRow);
+            }
+        });
     }
 }
