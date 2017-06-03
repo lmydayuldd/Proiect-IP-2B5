ca9464359a2d1b831221f5342e72b0e5e7f02943
Alexandru Olanuta <alejandro_alex96@yahoo.com>
2017-06-03 16:14:10 +0300

    Adaugare fisiere proiect aproape final

---
+++ Deserialize/Assets/showMenu2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showMenu2 : MonoBehaviour
+{
+   /* List<string> etaj = new List<string>() { "Alege Etaj", "Parter", "Etaj 1", "Etaj 2" };
+
+    public Dropdown dd_3d_etaj;
+    public Dropdown dd_2d_etaj;
+
+    public Dropdown dd_2d_camere;
+
+    public void showCamera(Dropdown dd)
+    {
+
+    }
+
+    public void Dropdown_IndexChanged_2D(int index)
+    {
+        Debug.Log("Click_2D: " + etaj[index]);
+    }
+
+
+    public void Dropdown_IndexChanged_3D(int index)
+    {
+        Debug.Log("Click_3D: " + etaj[index]);
+    }
+    */
+    void Start()
+    {
+        //Populare();
+    }
+    /*
+    void Populare()
+    {
+        dd_3d_etaj.AddOptions(etaj);
+        dd_2d_etaj.AddOptions(etaj);
+        dd_2d_camere.AddOptions(Deserialize.getCamere());
+    }
+
+    void Update()
+    {
+        //dd_3d_etaj.onValueChanged.AddListener(delegate { showCamera(dd_2d_etaj); });
+    }*/
+}
