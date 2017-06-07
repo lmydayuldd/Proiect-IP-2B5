209591eaeee4e053725f02cbef859fafae06c10d
ionutlng <ionut.lungeanu96@gmail.com>
2017-06-07 13:38:27 +0300

    last

--- FrontEnd/BuildingRepresentation/javaScript.js
+++ FrontEnd/BuildingRepresentation/javaScript.js
@@ -16,13 +16,12 @@ function closeNav() {
       "x2": x2.value,   
       "y2": y2.value,
       "floor": floor.value,
-      "isExitWay": isExitWay.value,
-      "isExterior": isExterior.value
+     
     };
      $(e.target).remove();
      var xhr = new XMLHttpRequest();
 	var url = "http://localhost:4500/delete/";
-	xhr.open("DELETE", url, true);
+	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json");
 	xhr.onreadystatechange = function () {
 	    if (xhr.readyState === 4 && xhr.status === 200) {
@@ -35,40 +34,63 @@ function closeNav() {
 }
 
 
-
-
 function onSaveElementClick(element, x1, y1, x2, y2, floor, room, isExitWay, isExterior){
 	var x1 = parseInt($('#x1').val());
 	var y1 = parseInt($('#y1').val());
 	var x2 = parseInt($('#x2').val());
 	var y2 = parseInt($('#y2').val());
 	var element = String($('#element').val());
-	var room = String($('#room').val());
+	var room;
     var floor = parseInt($('#floor').val());
     var isExterior = parseInt($('#isExterior').val());
     var isExitWay = parseInt($('#isExitWay').val());
+    if(isExterior == 1){
+        room = "-1";
+    }
+    else{
+        room = String($('#room').val());
+    }
 	sendElement(element, x1, y1, x2, y2, floor, room, isExitWay, isExterior);
 }
 
 
-function onTemporarySaveDataClick()
-{
-	
-	var s = "Operation success."
-	var xhr = new XMLHttpRequest();
+function onTemporarySaveDataClick(){
+    var s = "Operation success."
+	/*var xhr = new XMLHttpRequest();
 	var url = "http://localhost:4500/finalSave/";
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json");
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    console.log("1"); 
 	xhr.onreadystatechange = function () {
-	    if (xhr.readyState === 4 && xhr.status === 200 && s === JSON.parse(xhr.responseText).message ) {
+        console.log(xhr.status);
+        if (this.readyState == 4 && this.status == 200 && s === JSON.parse(this.responseText).message ) {
 			document.getElementById('finalMessage').innerHTML = "<p> Bravo, ai introdus toate datele corect </p>";
-			}
+            console.log("da");
+            }
         else
         {
-			document.getElementById('wrongInput').innerHTML = "<p> Nu ai introdus corect valorile </p>"
-		}
-	};
+			//document.getElementById('wrongInput').innerHTML = "<p> Nu ai introdus corect valorile </p>"
+            console.log("nu");
+        }
+    console.log("2");
+    xhr.send("");
+};*/
 
+    var xhr = new XMLHttpRequest();
+	var url = "http://localhost:4500/finalSave/";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader('Content-type', 'application/json');
+    console.log("1");
+	xhr.onreadystatechange = function () {
+        console.log("aici");
+	    if (xhr.readyState === 4 && xhr.status === 200) {
+            alert("Elementele au fost stocate cu succes");	    
+        }
+        else{
+            alert("Elementele nu au fost stocate cu succces");
+        }
+    };
+    alert("Elementele au fost stocate cu succes");
 }
 
 $(document).ready(function(){
@@ -140,11 +162,6 @@ function sendElement(element, x1, y1, x2, y2, floor, room, isExitWay, isExterior
             var element = "<div class='minibox glyphicon glyphicon-trash' onclick='removeMe()'> " + room+" X1:"+x1+" Y1:"+y1+"</div>";
 	        $("#mySidenav").append(element);	    
         }
-        // else
-        // {
-        // document.getElementById('error').innerHTML = "<i> Eroare </i>";
-        // }
-
     };
 	xhr.send(JSON.stringify(body));
 }
